0566245f4b1bc245a2d8409f74bc8c8c74ed1fdb
sxclij <sxclij@gmail.com>
2024-09-06 07:59:03 +0000

    struct json

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+struct json {
+    std::string key;
+    enum {
+        json_kind_num,
+        json_kind_str,
+        json_kind_vec,
+        json_kind_json,
+    } value_kind;
+    union {
+        int64_t* num;
+        std::string* str;
+        std::vector<struct json*> vec;
+        struct json* json;
+    }value;
+};
+
+struct json* parse(std::string src) {
+    struct json* root = (struct json*)malloc(sizeof(struct json));
+}
 
 int main() {
-    printf("hello world");
+    struct json* json = parse("{\"foo_root\":{\"foo_child\":1}}");
 }
