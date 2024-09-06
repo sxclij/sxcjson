05171347df01e67aee7644148651b8a5c39ccf88
sxclij <sxclij@gmail.com>
2024-09-06 08:28:29 +0000

    rm

--- main.cpp
+++ main.cpp
@@ -11,23 +11,7 @@ struct json {
     struct json* next;
 };
 
-struct json* parse_obj(std::string* src, uint32_t* i) {
-    struct json* kari = (struct json*)malloc(sizeof(struct json));
-    if ((*src)[*i] == '{') {
-    }
-    if ((*src)[*i] == '}') {
-    }
-    if ((*src)[*i] == ',') {
-        kari->next = parse_obj(src, i);
-        ;
-    }
-    if ((*src)[*i] == ':') {
-    }
-}
-struct json* parse(std::string src) {
-    uint32_t i = 0;
-    return parse_obj(&src, &i);
-}
+
 
 int main() {
     struct json* json = parse("{\"foo_root\":{\"foo_child\":1}}");
