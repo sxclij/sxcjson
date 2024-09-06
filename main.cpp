6b430761b8036d9d2bfecf99a20fd5fdfc1a36f5
sxclij <sxclij@gmail.com>
2024-09-06 08:23:49 +0000

    kari

--- main.cpp
+++ main.cpp
@@ -1,37 +1,44 @@
 #include <bits/stdc++.h>
 
-struct json {
-    enum {
+struct json
+{
+    enum
+    {
         json_kind_null,
         json_kind_str,
         json_kind_obj
     };
     std::string data;
-    struct json* obj;
-    struct json* next;
+    struct json *obj;
+    struct json *next;
 };
 
-struct json* parse_obj(std::string* src, uint32_t* i) {
-    if((*src)[*i] == '{') {
-
+struct json *parse_obj(std::string *src, uint32_t *i)
+{
+    struct json *kari = (struct json *)malloc(sizeof(struct json));
+    if ((*src)[*i] == '{')
+    {
     }
-    if((*src)[*i] == '}') {
-        
+    if ((*src)[*i] == '}')
+    {
     }
-    if((*src)[*i] == ',') {
-        
+    if ((*src)[*i] == ',')
+    {
+        kari->next = parse_obj(src, i);
+        ;
     }
-    if((*src)[*i] == ':') {
-        
+    if ((*src)[*i] == ':')
+    {
     }
 }
-struct json* parse(std::string src) {
-    struct json* root = (struct json*)malloc(sizeof(struct json));
+struct json *parse(std::string src)
+{
     uint32_t i = 0;
-    parse_obj(&src, &i);
+    return parse_obj(&src, &i);
 }
 
-int main() {
-    struct json* json = parse("{\"foo_root\":{\"foo_child\":1}}");
+int main()
+{
+    struct json *json = parse("{\"foo_root\":{\"foo_child\":1}}");
     printf("test");
 }
