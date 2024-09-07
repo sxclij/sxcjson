554c28702b6f7943c3eee3a06249184e965e5513
sxclij <sxclij@gmail.com>
2024-09-07 04:46:04 +0000

    nest

--- main.c
+++ main.c
@@ -10,23 +10,32 @@ struct sxcjson {
 
 struct sxcjson* parse_obj(const char* src, uint32_t* i) {
     struct sxcjson* result = (struct sxcjson*)malloc(sizeof(struct sxcjson));
-    if (src[*i] == ':') {
-        (*i)++;
-        parse_obj(src, i);
-    } else {
-        uint32_t start = i;
-        while (*i != '{' && *i != '}' && *i != ':' && *i != ',') {
-            (*i)++;
-        }
-        uint32_t str_size = *i - start;
-        char* str = (char*)malloc(sizeof(char) * (str_size + 1));
-        result->key = str;
-        result->next = NULL;
-        result->val = NULL;
-        memcpy(str, src + start, str_size);
-        str[str_size] = '\0';
+    uint32_t start = i;
+    if (src[*i] == '{') {
         return result;
     }
+    if (src[*i] == '}') {
+        return result;
+    }
+    if (src[*i] == ',') {
+        return result;
+    }
+    while (*i != '{' && *i != '}' && *i != ':' && *i != ',') {
+        (*i)++;
+    }
+    uint32_t str_size = *i - start;
+    char* str = (char*)malloc(sizeof(char) * (str_size + 1));
+    result->key = str;
+    result->next = NULL;
+    result->val = NULL;
+    memcpy(str, src + start, str_size);
+    str[str_size] = '\0';
+    if (src[*i] != ':') {
+        return result;
+    }
+    (*i)++;
+    result->val = parse_obj(src, i);
+    return result;
 }
 struct sxcjson* parse(const char* src) {
     uint32_t i = 0;
