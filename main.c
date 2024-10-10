a80a213ec38b4cea86ddbc67db28b63b0faffe16
sxclij <sxclij@gmail.com>
2024-10-10 15:23:48 +0900

    .

--- main.c
+++ main.c
@@ -10,6 +10,10 @@ struct sxcjson {
     struct sxcjson* child;
     struct sxcjson* next;
 };
+struct sxcjson_string {
+    char* data;
+    uint32_t size;
+};
 struct {
     char sxcjson_char[sxcjson_capacity];
     struct sxcjson sxcjson_data[sxcjson_capacity];
